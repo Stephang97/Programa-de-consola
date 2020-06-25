3ce80dd4fae446fa90c236ac493bcb3879524318
Stephang97 <52188073+Stephang97@users.noreply.github.com>
2020-06-24 19:22:24 -0500

    Update console.cs

--- console.cs
+++ console.cs
@@ -14,19 +14,19 @@ namespace AplicacionDeConsola
             // 'WriteLine' se utiliza para generar (imprimir) valores
 
             Console.WriteLine("Ingrese el primer número y presione enter"); // ingresamos el primer numero
-            numero1 = Convert.ToInt32(Console.ReadLine());  // 'Convert.ToInt32' convierte un dato tipo string en un dato tipo number 
+            numero1 = Convert.ToInt32(Console.ReadLine());  // 'Convert.ToInt32' convierte un dato de tipo string en un dato tipo number 
             // 'ReadLine' Lee la linea y obtiene la entrada 
 
             Console.WriteLine("Ingrese el segundo número y presione enter");    // ingresamos el segundo numero
             numero2 = Convert.ToInt32(Console.ReadLine()); 
             
-            Console.WriteLine("Ingrese el tercer número y preione enter");  //  ingresamos el tercer numero
+            Console.WriteLine("Ingrese el tercer número y presione enter");  //  ingresamos el tercer numero
             numero3 = Convert.ToInt32(Console.ReadLine());
             
              Console.WriteLine($"El resultado es: {numero1} + {numero2} + {numero3} = " + (numero1 + numero2 + numero3));  //concatenamos nuestras variables para realizar la suma
 
             Console.WriteLine("Presione enter para cerrar el programa")
-            Console.ReadKey();  // 'ReadKey toma la tecla preionada qu en este caso seria enter y termina el programa'
+            Console.ReadKey();  // 'ReadKey toma la tecla preionada que en este caso seria enter y termina el programa'
         }
     }
 }
